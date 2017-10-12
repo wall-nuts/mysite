84322e237b0390b6a08fadea0ac527c6f386189d
wall-nuts <448769943@qq.com>
2017-10-12 17:33:16 +0800

    mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "dj",
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST':'123.206.71.232',
+        'PORT':'3306',
     }
 }
 
